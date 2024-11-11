03966ec3b51e7296e893259ad93744cb1083911f
persons_giga_ide <doljenkoalex@gmail.com>
2024-11-11 21:10:42 +0300

    Соозданы контроллеры и представления для сущностей person и role. Проведено тестирование на данных, созданных в проете.

---
+++ src/main/java/controller/RoleServlet.java
@@ -0,0 +1,58 @@
+package controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class RoleServlet
+ */
+@WebServlet("/role")
+public class RoleServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RoleServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        String userPath;
+
+/*
+        PrintWriter writer = response.getWriter();
+        try {
+            writer.println("<h2>Привет ServletRole</h2>");
+        } finally {
+            writer.close();
+        }
+    }
+    */
+       userPath = request.getServletPath();
+        if("/role".equals(userPath)){
+            request.getRequestDispatcher("/views/role.jsp").forward(request, response);
+        }
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        doGet(request, response);
+    }
+
+}
+
