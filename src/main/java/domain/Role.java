4c4efec6705d1f28c2b1c8bfa4c051444991d40f
persons_giga_ide <doljenkoalex@gmail.com>
2024-11-11 22:18:53 +0300

    Изменён класс role.java - применены аннотации Lombok и Persistence.

--- src/main/java/domain/Role.java
+++ src/main/java/domain/Role.java
@@ -1,47 +1,23 @@
 package domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+
 /**
  *   Класс данных о должностях
  */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Role {
 
     // Идентификатор должности
+    @Id
     private Long id;
 
     // Наименование должности
     private String namerole;
-
-    public Role() {
-    }
-
-    public Role(String namerole) {
-        this.namerole = namerole;
-    }
-
-    public Role(Long id, String namerole) {
-        this.id = id;
-        this.namerole = namerole;
-    }
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNamerole() {
-        return namerole;
-    }
-
-    public void setNamerole(String namerole) {
-        this.namerole = namerole;
-    }
-
-    @Override
-    public String toString() {
-        return "Role {" + "Id = " + id + ", NameRole = " + namerole + "}";
-    }
 }
